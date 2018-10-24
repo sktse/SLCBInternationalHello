69d63c54af7a8c868c69f730942a46f0f605e64c
sktse <developer.dingo@gmail.com>
2018-10-23 23:57:14 -0400

    Bumps version to v1.1.0.

--- SLCBInternationalHello/SLCBInternationalHello_StreamlabsSystem.py
+++ SLCBInternationalHello/SLCBInternationalHello_StreamlabsSystem.py
@@ -20,7 +20,7 @@ ScriptName = "International Hello"
 Website = "https://github.com/sktse"
 Description = "Hello! Is it me you are looking for?"
 Creator = "sktse"
-Version = "1.0.1"
+Version = "1.1.0"
 
 #---------------------------
 #   Define Global Variables
