5d42483b0095067cfc39d66321dba2d0aff8fe49
sktse <developer.dingo@gmail.com>
2018-11-18 14:13:07 -0500

    Refactors constant greetings out.

--- SLCBInternationalHello/SLCBInternationalHello_StreamlabsSystem.py
+++ SLCBInternationalHello/SLCBInternationalHello_StreamlabsSystem.py
@@ -27,59 +27,7 @@ Version = ScriptConstants.VERSION
 #---------------------------
 script_settings = ScriptSettings()
 global Greetings
-Greetings = [
-    "Hello",
-    "Greetings",
-    "Hi",
-    "Hey",
-    "Allo",  # French
-    "Bonjour",  # French
-    "Top-o-the-morning",
-    "Hola",  # Spanish
-    "Ciao",  # Italian
-    "Buongiorno",  # Italian
-    "Hallo",  # German
-    "Guten tag",  # German
-    "Moin moin",  # German
-    "Namaste",  # Hindi
-    "Salaam",  # Farsi
-    "Merhaba",  # Turkish
-    "Szia",  # Hungarian
-    "Hej",  # Swedish, Danish
-    "Zdravo",  # Croatian
-    "Ahoj",  # Czech
-    "Kamusta",  # Flipino
-    "Hei",  # Finnish, Norwegian
-    "God dag",  # Norwegian
-    "Halo",  # Indonesian
-    "Sveiki",  # Latvian
-    "Salut",  # Romanian, etc
-    "Ahoj",  # Slovak
-    "Sawubona",  # Zulu
-    "Hullo",  # Scottish
-    "Dia dhuit",  # Gaelic
-    "G'day",  # Australian
-    "Aloha",  # Hawaiian
-    "Tere",  # Estonian
-    "Howdy",  # American
-    u'xin ch\xe0o',  # Vietnamese
-    u'Dzie\u0144 dobry',  # Polish
-    u'Ol\xe1',  # Portugese
-    u'\u0e2a\u0e27\u0e31\u0e2a\u0e14\u0e35',  # Thai - swasdi
-    u'\u3053\u3093\u306b\u3061\u306f',  # Japanese - Kon'nichiwa
-    u'\u3082\u3057\u3082\u3057',  # Japanese - Moshi Moshi
-    u'\uc5ec\ubcf4\uc138\uc694',  # Korean - yeoboseyo
-    u'\u0421\u0430\u0439\u043d \u0443\u0443',  # Mongolian - Sain uu
-    u'\u0421\u04d9\u043b\u0435\u043c\u0435\u0442\u0441\u0456\u0437 \u0431\u0435',  # Kazakh - Salemetsiz be
-    u'\u041f\u0440\u0438\u0432\u0435\u0442',  # Russian - Privet
-    u'\u4f60\u597d',  # Chinese - Ni Hao
-    u'P\xebrsh\xebndetje',  # Albanian
-    u'\u1230\u120b\u121d',  # Amharic - selami
-    u'\u0645\u0631\u062d\u0628\u0627',  # Arabic - marhabaan
-    u'\u03b3\u03b5\u03b9\u03b1 \u03c3\u03b1\u03c2',  # Greek - geia sas
-    u'Ked\u1EE5',  # Igbo - Nigeria
-    u'Bawo ni',  # Yoruba - Nigeria
-]
+Greetings = []
 global InputGreetings
 InputGreetings = []
 global CustomOutputGreetings
@@ -110,6 +58,9 @@ def Init():
 
 
 def initialize_input_greetings():
+    del Greetings[:]
+    Greetings.extend(ScriptConstants.DEFAULT_GREETINGS)
+
     # Delete the contents of the array but NOT creating a new instance
     # The pointer needs to be the same, but the contents nuked.
     del InputGreetings[:]
--- SLCBInternationalHello/lib/constants.py
+++ SLCBInternationalHello/lib/constants.py
@@ -8,3 +8,57 @@ class ScriptConstants:
 
     # Custom constants
     SCRIPT_KEY = "sktse-HelloReply"
+
+    DEFAULT_GREETINGS = [
+        "Hello",
+        "Greetings",
+        "Hi",
+        "Hey",
+        "Allo",  # French
+        "Bonjour",  # French
+        "Top-o-the-morning",
+        "Hola",  # Spanish
+        "Ciao",  # Italian
+        "Buongiorno",  # Italian
+        "Hallo",  # German
+        "Guten tag",  # German
+        "Moin moin",  # German
+        "Namaste",  # Hindi
+        "Salaam",  # Farsi
+        "Merhaba",  # Turkish
+        "Szia",  # Hungarian
+        "Hej",  # Swedish, Danish
+        "Zdravo",  # Croatian
+        "Ahoj",  # Czech
+        "Kamusta",  # Flipino
+        "Hei",  # Finnish, Norwegian
+        "God dag",  # Norwegian
+        "Halo",  # Indonesian
+        "Sveiki",  # Latvian
+        "Salut",  # Romanian, etc
+        "Ahoj",  # Slovak
+        "Sawubona",  # Zulu
+        "Hullo",  # Scottish
+        "Dia dhuit",  # Gaelic
+        "G'day",  # Australian
+        "Aloha",  # Hawaiian
+        "Tere",  # Estonian
+        "Howdy",  # American
+        u'xin ch\xe0o',  # Vietnamese
+        u'Dzie\u0144 dobry',  # Polish
+        u'Ol\xe1',  # Portugese
+        u'\u0e2a\u0e27\u0e31\u0e2a\u0e14\u0e35',  # Thai - swasdi
+        u'\u3053\u3093\u306b\u3061\u306f',  # Japanese - Kon'nichiwa
+        u'\u3082\u3057\u3082\u3057',  # Japanese - Moshi Moshi
+        u'\uc5ec\ubcf4\uc138\uc694',  # Korean - yeoboseyo
+        u'\u0421\u0430\u0439\u043d \u0443\u0443',  # Mongolian - Sain uu
+        u'\u0421\u04d9\u043b\u0435\u043c\u0435\u0442\u0441\u0456\u0437 \u0431\u0435',  # Kazakh - Salemetsiz be
+        u'\u041f\u0440\u0438\u0432\u0435\u0442',  # Russian - Privet
+        u'\u4f60\u597d',  # Chinese - Ni Hao
+        u'P\xebrsh\xebndetje',  # Albanian
+        u'\u1230\u120b\u121d',  # Amharic - selami
+        u'\u0645\u0631\u062d\u0628\u0627',  # Arabic - marhabaan
+        u'\u03b3\u03b5\u03b9\u03b1 \u03c3\u03b1\u03c2',  # Greek - geia sas
+        u'Ked\u1EE5',  # Igbo - Nigeria
+        u'Bawo ni',  # Yoruba - Nigeria
+    ]
